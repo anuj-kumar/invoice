30e4fb2be05c7a3f6dfc3bd098f76058db1ce368
Anuj Kumar <rockerboy.anuj@gmail.com>
2013-06-12 19:49:53 +0530

    Login and Permissions Implemented

--- fuel/app/classes/controller/base.php
+++ fuel/app/classes/controller/base.php
@@ -1,22 +1,34 @@
 <?php
 
+use \Fuel\Core\Response;
+use \Fuel\Core\Session;
+
 class Controller_Base extends Controller_Template {
-    /*protected function action_verify($module) {
-        $rights = Model_Access_Right::find($user_id);
-        Input::post();
-        
-    }*/
-    
-    protected static function dologin($user) {
+
+    protected function access($module) {
+        $user = Session::get('user');
+        if ($user) {
+            $right = Model_Access_Right::find('first', array(
+                        'where' => array('user_id' => $user->id),
+                            )
+            );
+            //print_r($right);
+            //echo $right->print_invoice;
+            return $right[$module];
+        }
+    }
+
+    protected static function do_login($user) {
         Session::set('user', $user);
         //$user->last_login_at =
-                echo date('Y-m-d') . time('HH:MM:SS');
+        echo date('Y-m-d') . time('HH:MM:SS');
 //        $user->save();
     }
-    
+
     protected function logout_user() {
-        \Fuel\Core\Session::destroy();
+        Session::destroy();
     }
+
 }
 
 ?>
