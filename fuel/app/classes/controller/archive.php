0ab95a81ff3d588beab5cbb72df2cff3afc653bb
Anuj Kumar <rockerboy.anuj@gmail.com>
2013-06-13 11:35:16 +0530

    Sort in Archive

--- fuel/app/classes/controller/archive.php
+++ fuel/app/classes/controller/archive.php
@@ -1,21 +1,21 @@
 <?php
 
-class Controller_Archive extends Controller_Template
-{
+class Controller_Archive extends Controller_Template {
 
-	public function action_view($sort = NULL)
-	{
-        $data['invoices'] = Model_Invoice::find('all');
-		$data["subnav"] = array('view'=> 'active' );
-		$this->template->title = 'Archive &raquo; View';
-		$this->template->content = View::forge('archive/view', $data);
-	}
+    public function action_view($sort = NULL) {
+        $data['invoices'] = Model_Invoice::find('all', array(
+                    'order_by' => $sort,
+                        )
+        );
+        $data["subnav"] = array('view' => 'active');
+        $this->template->title = 'Archive &raquo; View';
+        $this->template->content = View::forge('archive/view', $data);
+    }
 
-	public function action_search()
-	{
-		$data["subnav"] = array('search'=> 'active' );
-		$this->template->title = 'Archive &raquo; Search';
-		$this->template->content = View::forge('archive/search', $data);
-	}
+    public function action_search() {
+        $data["subnav"] = array('search' => 'active');
+        $this->template->title = 'Archive &raquo; Search';
+        $this->template->content = View::forge('archive/search', $data);
+    }
 
 }
