1e3f203ae9ff37eba5f645d47f68043043057238
aaghran <aaghran@gmail.com>
2013-06-14 15:40:51 +0530

    archives view and template added

--- fuel/app/classes/controller/archive.php
+++ fuel/app/classes/controller/archive.php
@@ -2,6 +2,14 @@
 
 class Controller_Archive extends Controller_Template {
 
+        public function action_index($sort = NULL) {
+        $data['invoices'] = Model_Invoice::find('all');
+        $data["subnav"] = array('view' => 'active');
+        $this->template->title = 'Archive &raquo; View';
+        $this->template->content = View::forge('archive/view', $data);
+    }
+
+    
     public function action_view($sort = NULL) {
         $data['invoices'] = Model_Invoice::find('all', array(
                     'order_by' => $sort,
