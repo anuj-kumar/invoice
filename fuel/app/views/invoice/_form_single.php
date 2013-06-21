c65d5b4b4d0491516c3bb0bbf9dadc37b5cffba5
aaghran <aaghran@gmail.com>
2013-06-22 01:16:04 +0530

    _from_single modified, margin-top reduced

--- fuel/app/views/invoice/_form_single.php
+++ fuel/app/views/invoice/_form_single.php
@@ -3,6 +3,16 @@
 <h4>Single Customer</h4>    
 <fieldset>
     <div class="grid-12-12 ">
+        <div class="grid-2-12 ">
+            
+            <label>Title : <em class="formee-req">*</em></label>
+            <select name="title" style="width: 60px" required>
+                <option>Dr.</option>
+                <option>Mr.</option>
+                <option>Mrs.</option>
+                <option>Ms.</option>
+            </select>
+        </div>
         <div class="grid-4-12 ">
             <label>First Name : <em class="formee-req">*</em></label>
             <input type="text" name="f_name" class="formee-large" placeholder="First Name" required autofocus>
@@ -12,7 +22,7 @@
             <input type="text" name="l_name" class="formee-large" placeholder="Last Name" required>
         </div>
     </div>  
-    <div class="grid-12-12 ">
+    <div class="grid-12-12 " style="margin-top: 0px">
         <div class="grid-4-12 ">
             <label>Address Line #1 : <em class="formee-req"></em></label>
             <input type="text" name="addr_1" class="formee-large" placeholder="Adress Line #1" autocomplete="off" required>
@@ -26,7 +36,7 @@
             <input type="text" name="addr_3" class="formee-large" placeholder="Address Line #3" autocomplete="off" >
         </div>
     </div>
-    <div class="grid-12-12 ">
+    <div class="grid-12-12 " style="margin-top: -20px">
         <div class="grid-4-12 ">
             <label>City : <em class="formee-req"></em></label>
             <input type="text" name="city" class="formee-large" placeholder="Adress Line #1" required>
@@ -42,13 +52,13 @@
   <option value="Tamil Nadu">
 </datalist>
         </div>
-        <div class="grid-4-12 ">
+        <div class="grid-4-12 " >
             <label>Pin Code : </label>
             <input type="text" name="pin" class="formee-large" placeholder="Address Line #3" >
         </div>
     </div>
     
-    <div class="grid-12-12 ">
+    <div class="grid-12-12 " style="margin-top: -20px">
         <div class="grid-4-12 ">
             <label>Telephone : <em class="formee-req">*</em></label>
             <input type='tele' name="tele" pattern='[\+]\d{2}\d{2}\d{4}\d{4}' placeholder='Tele: +99(99)9999-9999' autocomplete="off" required>
